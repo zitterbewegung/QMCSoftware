6df124bdd987dd3a3df08ac1e0717e7d52252576
Fred J Hickernell <hickernell@iit.edu>
2021-07-20 18:11:32 -0500

    Fred's Office Computer Updates

--- LR1.py
+++ LR1.py
@@ -2,22 +2,26 @@ from qmcpy.integrand.LR import LR
 from sys import meta_path
 import numpy
 from qmcpy import *
-data = numpy.genfromtxt('binary.csv', dtype=float, delimiter=',', names = True)
-print(data)
-s = numpy.array([[.40]])
-t = numpy.array([1])
+data = numpy.genfromtxt('binary.csv', dtype=float, delimiter=',', skip_header = True)
+# print(data)
+
+mdata = 4
+s = data[:mdata,1:]
+t = data[:mdata,0]
+prior_variance = [1,1e-4,1,1]
 
 no, dim = s.shape
+# print(s.shape)
 
-k = LR(IIDStdUniform(dim+1,seed=8), s_matrix = s, t = t)
+k = LR(IIDStdUniform(dim+1,seed=8), s_matrix = s, t = t, prior_variance = prior_variance)
 solution, data = CubMCCLT(k, abs_tol = .001).integrate()
 print(data)
 print(" ")
-k1 = LR(Sobol(dim+1,seed=8), s_matrix = s, t = t)
+k1 = LR(Sobol(dim+1,seed=8), s_matrix = s, t = t,  prior_variance = prior_variance)
 solution1, data1 = CubQMCSobolG(k1, abs_tol = .001).integrate()
 print(data1)
 print(" ")
-my_instance = LR(sampler = Sobol(dimension=dim+1, seed = 7), s_matrix = s, t = t)
+my_instance = LR(sampler = Sobol(dimension=dim+1, seed = 7), s_matrix = s, t = t, prior_variance = prior_variance)
 p = my_instance.discrete_distrib.gen_samples(n_min=0, n_max=1024)
 y = my_instance.f(p)
 print(y.mean())
