b7bfd0351150a567145bbfdc390aff1394db4a5c
CDHJ2000 <cdhjr2000@gmail.com>
2021-07-22 18:47:44 -0700

    Logistic Regression

--- LR1.py
+++ LR1.py
@@ -2,13 +2,19 @@ from qmcpy.integrand.LR import LR
 from sys import meta_path
 import numpy
 from qmcpy import *
-data = numpy.genfromtxt('binary.csv', dtype=float, delimiter=',', names = True)
-print(data)
-s = numpy.array([[.40]])
-t = numpy.array([1])
+data = numpy.genfromtxt('binary.csv', dtype=float, delimiter=',', skip_header = True)
+
+n = 10
+
+s = data[:n, 1:]
+t = data[:n, 0]
 
 no, dim = s.shape
 
+prior_variance = [1,1e-4,1,1]
+
+
+"""
 k = LR(IIDStdUniform(dim+1,seed=8), s_matrix = s, t = t)
 solution, data = CubMCCLT(k, abs_tol = .001).integrate()
 print(data)
@@ -17,7 +23,15 @@ k1 = LR(Sobol(dim+1,seed=8), s_matrix = s, t = t)
 solution1, data1 = CubQMCSobolG(k1, abs_tol = .001).integrate()
 print(data1)
 print(" ")
+
 my_instance = LR(sampler = Sobol(dimension=dim+1, seed = 7), s_matrix = s, t = t)
 p = my_instance.discrete_distrib.gen_samples(n_min=0, n_max=1024)
 y = my_instance.f(p)
 print(y.mean())
+"""
+k = LR(Sobol(dim+1,seed=8), s_matrix = s, t = t)
+solution, data = CubQMCCLT(k, abs_tol = .001).integrate()
+print(data)
+print(data.error_bound)
+print(" ")
+print(solution)
--- qmcpy/integrand/LR.py
+++ qmcpy/integrand/LR.py
@@ -12,29 +12,25 @@ class LR(Integrand):
         self.true_measure = Gaussian(sampler, mean=prior_mean, covariance = prior_variance)
         s_matrix2 = array(s_matrix)
         m, d = s_matrix2.shape
-        check1 = True
         if m != len(t):
-            check1 = False
-        if check1 == True:
-            c = ones((len(t), 1))
-            self.s = column_stack((c, s_matrix))
-        else:
-            print("s_matrix must have the same amount of rows as the amount of elements in t")
-        check = True
-        for i in range(len(t)):
-            if 0 != t[i] and t[i] != 1:
-                check = False
-        if check == True:
-            self.t = t
-        else:
-            print("for all 't_i', t_i can only equal 0 or 1")
-        self.dprime = 1
+            ParameterError("s_matrix must have the same amount of rows as the amount of elements in t")
+        c = ones((len(t), 1))
+        self.s = column_stack((c, s_matrix))
+        if ((t != 0)&(t != 1)).any():
+            ParameterError("for all 't_i', t_i can only equal 0 or 1")
+        self.t = t
+        self.dprime = 2
         super(LR, self).__init__()
         
-    def g(self, x):
+    def g(self, x, compute_flags):
         st = self.s.transpose()
         z = x@st
         z1 = z*self.t
         matrix = exp(z1)/(1+exp(z))
         matrix1 = prod(matrix, axis=1)
-        return matrix1
+        num = x[:, 0]*matrix1
+        n,d=x.shape
+        y=zeros((n,2),dtype=float)
+        y[:,0] = matrix1
+        y[:,1] = num
+        return y
