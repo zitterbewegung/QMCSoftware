b4f6bfa954702396a082076a433ee310de493c1c
CDHJ2000 <cdhjr2000@gmail.com>
2021-07-27 15:57:21 -0700

    Logistic Regression

--- qmcpy/integrand/LR.py
+++ qmcpy/integrand/LR.py
@@ -8,7 +8,7 @@ from numpy import *
 
 class LR(Integrand):
 
-    def __init__(self, sampler, s_matrix, t, prior_mean = 0, prior_variance = 1):
+    def __init__(self, sampler, s_matrix, t, r = 0, prior_mean = 0, prior_variance = 1):
         self.true_measure = Gaussian(sampler, mean=prior_mean, covariance = prior_variance)
         s_matrix2 = array(s_matrix)
         m, d = s_matrix2.shape
@@ -19,18 +19,18 @@ class LR(Integrand):
         if ((t != 0)&(t != 1)).any():
             ParameterError("for all 't_i', t_i can only equal 0 or 1")
         self.t = t
-        self.dprime = 2
+        self.r = r
+        self.dprime = r+1
         super(LR, self).__init__()
         
-    def g(self, x, compute_flags):
+    def g(self, x, **kwargs):
         st = self.s.transpose()
         z = x@st
         z1 = z*self.t
         matrix = exp(z1)/(1+exp(z))
         matrix1 = prod(matrix, axis=1)
-        num = x[:, 0]*matrix1
         n,d=x.shape
-        y=zeros((n,2),dtype=float)
+        y=zeros((n,self.dprime),dtype=float)
         y[:,0] = matrix1
-        y[:,1] = num
+        y[:,1:] = x[:, 0:self.r]*matrix1
         return y
