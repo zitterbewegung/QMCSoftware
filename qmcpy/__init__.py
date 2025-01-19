f355b7b2fd76cd14baaae10e1ab6aa76f362f72c
Joshua Herman <30265+zitterbewegung@users.noreply.github.com>
2025-01-19 09:04:13 -0600

    Update __init__.py
    Removing the ability to check of latex is installed.

--- qmcpy/__init__.py
+++ qmcpy/__init__.py
@@ -10,34 +10,11 @@ import importlib.resources as pkg_resources
 import subprocess
 import sys
 
-def is_latex_installed():
-  """
-  Checks if LaTeX is installed on the system.
-
-  Returns:
-    True if LaTeX is installed, False otherwise.
-  """
-  try:
-    if sys.platform == "win32":
-      # Check for MikTex on Windows
-      subprocess.run(["where", "latex"], capture_output=True, check=True)
-    elif sys.platform == "darwin":
-      # Check for MacTex on macOS
-      subprocess.run(["which", "latex"], capture_output=True, check=True)
-    else:  # Linux and other Unix-like systems
-      subprocess.run(["which", "pdflatex"], capture_output=True, check=True)
-    return True
-  except (FileNotFoundError, subprocess.CalledProcessError):
-    return False
 
 def qmc_apply_style():
     """Apply the qmcpy matplotlib style to allow for a standard style."""
     style_file = 'qmcpy.mplstyle'
-
-    if is_latex_installed:
-        print("Please install latex it is required for this style")
-        return
-    
+  
     try:
         with pkg_resources.path(__package__, style_file) as style_path:
             plt.style.use(style_path)
